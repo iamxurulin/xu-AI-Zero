c90ffef9cec5b635a189228924db07d5c7fdbd1f
iamxurulin <53854275+iamxurulin@users.noreply.github.com>
2026-01-26 16:15:04 +0800

    feat: 后端初始化，在controller包下新建一个测试接口

---
+++ xu-AI-Zero-backend/src/main/java/com/ruhuo/xuaizerobackend/XuAiZeroBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ruhuo.xuaizerobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuAiZeroBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuAiZeroBackendApplication.class, args);
+    }
+
+}
---
+++ xu-AI-Zero-backend/src/test/java/com/ruhuo/xuaizerobackend/XuAiZeroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruhuo.xuaizerobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuAiZeroBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
