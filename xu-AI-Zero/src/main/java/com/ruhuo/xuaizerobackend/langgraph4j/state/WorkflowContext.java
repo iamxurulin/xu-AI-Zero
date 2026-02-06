909515aa76fcf243508f05f6ff76a201936c4dc2
iamxurulin <53854275+iamxurulin@users.noreply.github.com>
2026-02-06 13:17:14 +0800

    feat: 后端：
    把每个图片收集工具定义成工作节点，并发执行这些工作节点，最后统一汇总结果。

--- xu-AI-Zero/src/main/java/com/ruhuo/xuaizerobackend/langgraph4j/state/WorkflowContext.java
+++ xu-AI-Zero/src/main/java/com/ruhuo/xuaizerobackend/langgraph4j/state/WorkflowContext.java
@@ -1,5 +1,6 @@
 package com.ruhuo.xuaizerobackend.langgraph4j.state;
 
+import com.ruhuo.xuaizerobackend.langgraph4j.model.ImageCollectionPlan;
 import com.ruhuo.xuaizerobackend.langgraph4j.model.ImageResource;
 import com.ruhuo.xuaizerobackend.langgraph4j.model.QualityResult;
 import com.ruhuo.xuaizerobackend.model.enums.CodeGenTypeEnum;
@@ -76,6 +77,23 @@ public class WorkflowContext implements Serializable {
      * 质量检查结果
      */
     private QualityResult qualityResult;
+
+    /**
+     * 图片收集计划
+     *
+     */
+    private ImageCollectionPlan imageCollectionPlan;
+
+    /**
+     * 并发图片收集的中间结果字段
+     */
+    private List<ImageResource> contentImages;
+
+    private List<ImageResource> illustrations;
+
+    private List<ImageResource> diagrams;
+
+    private List<ImageResource> logos;
 
 
     @Serial
